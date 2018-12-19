0945d735cc5d8a27606a5ebcb29f45a1f24394b2
Tucker <trblackw@ncsu.edu>
2018-12-19 11:43:58 -0500

    pagination and sort functionality complete

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
+const Review = require("../models/review");
 
 router.get("/test", (req, res) => {
   res.send("Connecting react and express!!!");
@@ -10,9 +11,9 @@ router.get("/generate-fake-data", (req, res) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product();
 
-    product.category = faker.commerce.department();
-    product.name = faker.commerce.productName();
-    product.price = faker.commerce.price();
+    product.category = faker.commerce.department().toLowerCase();
+    product.name = faker.commerce.productName().toLowerCase();
+    product.price = faker.commerce.price().toLowerCase();
     product.image =
       "https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png";
 
@@ -23,12 +24,12 @@ router.get("/generate-fake-data", (req, res) => {
   res.end();
 });
 
-router.get("/products", (req, res, next) => {
-  Product.find({}).exec((err, products) => {
-    if (err) return next(err);
-    res.send(products);
-  });
-});
+// router.get("/products", (req, res, next) => {
+//   Product.find({}).exec((err, products) => {
+//     if (err) return next(err);
+//     res.send(products);
+//   });
+// });
 
 router.get("/products/:page?/:category?/:sort?", (req, res, next) => {
   const query = {};
@@ -36,61 +37,64 @@ router.get("/products/:page?/:category?/:sort?", (req, res, next) => {
     query[key] = req.query[key];
   }
   let { page, category, sort } = query;
-  //hacky temporary solution
-  if (category) {
-    category = `${category.charAt(0).toUpperCase()}${category
-      .split("")
-      .slice(1)
-      .join("")}`;
-    return category;
+
+  if (Number(page) <= 0) {
+    const response = {
+      error: true,
+      guidance: "Invalid page number--page must be 1 or greater"
+    };
+    //  res.sendStatus(404);
+    return res.json(response);
   }
 
   if (sort) {
-    if (sort.toLowerCase() === "highest") {
+    if (sort === "highest") {
       sort = "descending";
-      return sort;
-    } else if (sort.toLowerCase() === "lowest") {
+    } else if (sort === "lowest") {
       sort = "ascending";
-      return sort;
     }
   }
 
   //just category
-  Product.where({ category })
+  Product.where({ category: { $regex: new RegExp(category), $options: "i" } })
     .populate("product")
     .limit(10)
+    .skip(10 * Number(page - 1))
     .sort({ price: sort })
     .exec((err, products) => {
       Product.countDocuments().exec((err, count) => {
-        if (err) return next(err);
+        if (err) {
+          //  res.sendStatus(404);
+          return next(err);
+        }
+        //   res.sendStatus(200);
         res.send(products);
       });
     });
-
-  //   Product.find({ category })
-  //     .populate("product")
-  //     .limit(10)
-  //     .exec((err, products) => {
-  //       Product.countDocuments().exec((err, count) => {
-  //         if (err) return next(err);
-  //         if (sort) {
-  //           if (sort.toLowerCase() === "highest") {
-  //             products = products.sort((a, b) => b.price - a.price);
-  //           } else {
-  //             products = products.sort((a, b) => a.price - b.price);
-  //           }
-  //         }
-  //         res.send(products);
-  //       });
-  //     });
 });
 
 //Returns a specific product by it's id
-router.get("/products/:product", (req, res) => {});
+router.get("/products/:product", (req, res) => {
+  const product = Product.findById(id).exec();
+  if (!product) {
+    res.sendStatus(404);
+  }
+  res.sendStatus(200);
+  res.send(product);
+});
 //Returns ALL the reviews, but limited to 40 at a time. This one will be a little tricky as you'll have to retrieve them out of the products. You should be able to pass in an options `page` query to paginate.
 router.get("/reviews", (req, res) => {});
 // Creates a new product in the database
-router.post("/products", (req, res) => {});
+router.post("/products", (req, res) => {
+  const { category, name, price } = req.body;
+  const product = Product.create({ category, name, price });
+
+  if (!product) {
+    res.sendStatus(404);
+  }
+  res.sendStatus(200);
+  res.send(product);
+});
 // Creates a new review in the database by adding it to the correct product's reviews array
 router.post("/:product/reviews", (req, res) => {});
 //Deletes a product by id
