8cd05daf5e35255e467eb6032e166e205442edda
Tucker <trblackw@ncsu.edu>
2018-12-20 17:10:00 -0500

    built out landing to render product pages with Filters component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useInputValue } from "../hooks/useInputValue";
+import { useToggle } from "../hooks/useToggle";
 import Nav from "./Nav";
 import Product from "./Product";
-import { ProductGrid } from "../styled_elements/index";
+import { LandingContainer } from "../styled_elements/layout";
+import Grid from "@material-ui/core/Grid";
+import Filters from "./Filters";
+import Button from "@material-ui/core/Button";
+// import ReactPaginate from "react-paginate";
 
 const Landing = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [open, toggle] = useToggle(false);
+  const [page, setPage] = useState(1);
+  const [category, setCategory] = useState("");
+  const [sort, setSort] = useState("");
 
   useEffect(() => {
-    callAPI().then(products => setProducts(products));
+    callAPI().then(([products, categories]) => {
+      setProducts(products);
+      setCategories(categories);
+    });
   }, []);
 
   const callAPI = async () => {
     try {
-      const res = await fetch("/products");
+      const res = await fetch(`/products/${page}`);
       const body = await res.json();
       if (res.status !== 200) throw Error(body.message);
       return body;
@@ -21,22 +34,28 @@ const Landing = () => {
       console.error(error);
     }
   };
-  console.log(products);
+  console.log(categories);
   return (
     <>
       <Nav />
-      <ProductGrid>
-        {products &&
-          products.map(({ _id, category, name, price, image }) => (
-            <Product
-              key={_id}
-              category={category}
-              name={name}
-              price={price}
-              image={image}
-            />
-          ))}
-      </ProductGrid>
+      {open && <Filters categories={categories} />}
+      <LandingContainer>
+        <Button variant="outlined" color="secondary" onClick={toggle}>
+          Refine search
+        </Button>
+        <Grid container={true} spacing={35} xs zeroMinWidth>
+          {products &&
+            products.map(({ _id, category, name, price, image }) => (
+              <Product
+                key={_id}
+                category={category}
+                name={name}
+                price={price}
+                image={image}
+              />
+            ))}
+        </Grid>
+      </LandingContainer>
     </>
   );
 };
