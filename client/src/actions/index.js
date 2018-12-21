0f4fa20cf47f62b23c23309c92483b48a427ed94
Tucker <trblackw@ncsu.edu>
2018-12-21 18:45:26 -0500

    refactored action to handle more product details

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,19 +1,34 @@
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
+export const CHANGE_PAGE = "CHANGE_PAGE";
 
-export const fetchProducts = () => {
+export const fetchProducts = (page = 1) => {
   return async dispatch => {
     try {
-      const res = await fetch(`/products`);
-      const [products, categories] = await res.json();
-      if (products && categories) {
+      const res = await fetch(`/products/${page}`);
+      const [productList, categories] = await res.json();
+      const { docs: products, total, pages } = productList;
+      if (products) {
         return dispatch({
           type: FETCH_PRODUCTS,
           products,
-          categories
+          categories,
+          total,
+          pages
         });
       }
     } catch (error) {
       console.error(error);
     }
   };
+};
+
+export const applyFilters = () => {
+  console.log("poop");
+  // return async dispatch => {
+  //    try {
+
+  //    } catch (error) {
+
+  //    }
+  // }
 };
