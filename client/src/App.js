7f8b93c410a22ebe5df24e13329af6309bff42b6
Tucker <trblackw@ncsu.edu>
2018-12-21 18:46:40 -0500

    added route for specific product page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
+import Page from "./components/Page";
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Landing} />
+      <Route path="/products/:page" component={Page} />
     </Switch>
   </Router>
 );
