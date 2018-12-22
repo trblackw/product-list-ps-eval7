eab4f21e0d61fdb9950e924e2b07c21b367e7bb9
Tucker <trblackw@ncsu.edu>
2018-12-22 14:44:13 -0500

    Refactored out homepage content into Productlist component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useInputValue } from "../hooks/useInputValue";
 import { useToggle } from "../hooks/useToggle";
-import { Redirect } from "react-router-dom";
 import Nav from "./Nav";
-import Product from "./Product";
 import { LandingContainer } from "../styled_elements/layout";
-import Grid from "@material-ui/core/Grid";
 import Filters from "./Filters";
 import Button from "@material-ui/core/Button";
-// import ReactPaginate from "react-paginate";
 import { connect } from "react-redux";
 import { fetchProducts } from "../actions/index";
 import { bindActionCreators } from "redux";
 import { Pages } from "../styled_elements/layout";
+import ProductList from "./ProductList";
 
 const Landing = ({ fetchProducts, products, total, pages }) => {
   const [open, toggle] = useToggle(false);
@@ -62,18 +58,7 @@ const Landing = ({ fetchProducts, products, total, pages }) => {
         >
           Refine search
         </Button>
-        <Grid container={true} spacing={32} zeroMinWidth>
-          {products &&
-            products.map(({ _id, category, name, price, image }) => (
-              <Product
-                key={_id}
-                category={category}
-                name={name}
-                price={price}
-                image={image}
-              />
-            ))}
-        </Grid>
+        <ProductList products={products} />
       </LandingContainer>
     </>
   );
