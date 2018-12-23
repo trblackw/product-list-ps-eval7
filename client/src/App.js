a0486ea6c9e69bab6c0129464a85e66b3e8f78f6
Tucker <trblackw@ncsu.edu>
2018-12-23 14:30:18 -0500

    extracted Nav component out to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import SelectedProduct from "./components/SelectedProduct";
+import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route path="/products/:product" component={SelectedProduct} />
-    </Switch>
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/products/:product" component={SelectedProduct} />
+      </Switch>
+    </>
   </Router>
 );
 
