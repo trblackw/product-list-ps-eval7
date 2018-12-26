efddfaf19a3487faf0c5dae467a36d53be0e19a9
Tucker <trblackw@ncsu.edu>
2018-12-26 10:30:34 -0500

    implemented client side product filtering by category

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import Product from "./Product";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-const ProductList = ({ products }) => (
-  <>
+const ProductList = ({ products, filteredProducts, filters }) => {
+  useEffect(
+    () => {
+      console.log(filters);
+    },
+    [products, filteredProducts]
+  );
+  return (
     <Grid container={true} spacing={32} zeroMinWidth>
-      {products &&
-        products.map(({ _id, category, name, price, image }) => (
-          <Product
-            key={_id}
-            id={_id}
-            category={category}
-            name={name}
-            price={price}
-            image={image}
-          />
-        ))}
+      {!filters
+        ? products &&
+          products.map(({ _id, category, name, price, image }) => (
+            <Product
+              key={_id}
+              id={_id}
+              category={category}
+              name={name}
+              price={price}
+              image={image}
+            />
+          ))
+        : filteredProducts.map(({ _id, category, name, price, image }) => (
+            <Product
+              key={_id}
+              id={_id}
+              category={category}
+              name={name}
+              price={price}
+              image={image}
+            />
+          ))}
     </Grid>
-  </>
-);
+  );
+};
 
-export default ProductList;
+const mapStateToProps = ({ productsReducer }) => ({
+  products: productsReducer.products,
+  filters: productsReducer.filters,
+  filteredProducts: productsReducer.filteredProducts
+});
+
+// const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps
+  // mapDispatchToProps
+)(ProductList);
