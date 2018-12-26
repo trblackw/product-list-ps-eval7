33160ce93579066e25e98838ffdde059ae47e74d
Tucker <trblackw@ncsu.edu>
2018-12-26 09:54:19 -0500

    added logo link to homepage

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { NavContainer } from "../styled_elements/index";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <NavContainer>
-      <h3>Products.io</h3>
+      <Link to="/">
+        <h3
+          style={{
+            color: "whitesmoke"
+          }}
+        >
+          Products.io
+        </h3>
+      </Link>
     </NavContainer>
   );
 };
